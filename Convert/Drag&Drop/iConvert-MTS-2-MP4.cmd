324c3a9d70015fca9ff4a5c7f88d0852bc498073
KaO <52933590+painteau@users.noreply.github.com>
2023-03-03 00:01:10 +0100

    Update iConvert-MTS-2-MP4.cmd

--- Convert/Drag&Drop/iConvert-MTS-2-MP4.cmd
+++ Convert/Drag&Drop/iConvert-MTS-2-MP4.cmd
@@ -4,7 +4,7 @@ set "original_file=%~f1"
 set "file_name=%~nx1"
 set "file_dir=%~dp1"
 cls
-ffmpeg -i "%original_file%" -vcodec copy -acodec copy "%targetfile%"
+ffmpeg -i "%original_file%" -c:v copy -c:a aac -strict experimental -b:a 128k "%targetfile%"
 pause
 
 
